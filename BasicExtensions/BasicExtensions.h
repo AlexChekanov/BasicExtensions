ee9a763e959d1fccfaa4766c0c955d18408e2ead
Alexey Chekanov <chekanov@hotmail.com>
2018-03-05 16:30:02 -0500

    Initial Commit

---
+++ BasicExtensions/BasicExtensions.h
@@ -0,0 +1,19 @@
+//
+//  BasicExtensions.h
+//  BasicExtensions
+//
+//  Created by Alexey Chekanov on 3/5/18.
+//  Copyright © 2018 Tips & Tricks, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BasicExtensions.
+FOUNDATION_EXPORT double BasicExtensionsVersionNumber;
+
+//! Project version string for BasicExtensions.
+FOUNDATION_EXPORT const unsigned char BasicExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicExtensions/PublicHeader.h>
+
+
